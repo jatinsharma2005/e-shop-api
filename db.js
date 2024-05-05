27c2a6bf200224783d881ab78ed7e303c74ea17a
jatin <jatinkumar20056@gmail.com>
2024-05-05 23:42:48 +0530

    a

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
-dotenv.config({ path: ".env" });
+dotenv.config({ path: "./.env" });
 const mongoose = require("mongoose");
 const mongoURI = process.env.MONGODB_URL;
 
